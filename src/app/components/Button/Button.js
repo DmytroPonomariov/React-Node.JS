ade0e74a74abacb0e21618195a0ccb583b4b1e9f
DmytroPonomariov <dima281998@gmail.com>
2024-12-13 17:29:26 +0000

    version1.0

---
+++ src/app/components/Button/Button.js
@@ -0,0 +1,15 @@
+const buttonStyle = {
+  color: 'rgb(18, 255, 18)',
+  backgroundColor: 'transparent',
+  border: '0px',
+  fontFamily: 'Courier',
+  fontSize: '33pt'  
+}
+
+
+export default function Button() {
+  return <button style={buttonStyle} data-testid="element-button">Wake up Neo...</button>
+}
+
+// export default Button
+
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+import Wrapper from "./components/Wrapper";
+
+export default function Home() {
+  return (
+  <Wrapper />
+);
+}
